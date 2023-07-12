b8f79ae6f68be0a9e41470f6cff5fb8129258d5f
Muhammad Farhan <muhammadfarhan012345@gmail.com>
2023-07-12 15:43:05 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+let ref;
+let lastTime;
+const debounce = (func, timer) => {
+  if (ref) {
+    clearTimeout(ref);
+    // ref = null;
+    // timer = Date.now() - lastTime;
+  }
+  ref = setTimeout(() => {
+    func();
+    lastTime = 0;
+    ref=null
+  }, timer);
+  lastTime = Date.now() + timer;
+};
+export default debounce;
